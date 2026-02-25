c266f60b3061457726b49f444eaa31998e9bee6f
zainab14-48 <s202251360@kfupm.edu.sa>
2026-02-25 11:55:08 +0300

    TaskItem

--- 5-4-react-rendering-lists/src/components/TaskItem.jsx
+++ 5-4-react-rendering-lists/src/components/TaskItem.jsx
@@ -5,23 +5,36 @@ export default function TaskItem({ task, onToggle, onDelete }) {
   return (
     <li className="task">
       <label className="taskMain">
+
         {/* TODO (TASK 4): Checkbox calls onToggle(task.id) on change
             Also make it reflect task.isDone using checked={task.isDone} */}
-        <input type="checkbox" />
 
-        {/* TODO (TASK 3): Show <DueBadge /> ONLY when task is NOT done (&&)
-            Example:
-              {!task.isDone && <DueBadge dueDate={task.dueDate} />} */}
+        <input
+          type="checkbox"
+          checked={task.isDone}
+          onChange={() => onToggle(task.id)}
+        />
 
-        {/* TODO (TASK 2): Display the task title
-            Example:
-              <span className="title">{task.title}</span> */}
+        {/* TODO (TASK 3): Show <DueBadge /> ONLY when task is NOT done (&&) */}
+
+        {!task.isDone && <DueBadge dueDate={task.dueDate} />}
+
+        {/* TODO (TASK 2): Display the task title */}
+
+        <span className="title">{task.title}</span>
+
       </label>
 
       {/* TODO (TASK 4): Delete button calls onDelete(task.id) on click */}
-      <button className="ghost" aria-label="Delete task">
+
+      <button
+        className="ghost"
+        aria-label="Delete task"
+        onClick={() => onDelete(task.id)}
+      >
         ✕
       </button>
+
     </li>
   );
 }
